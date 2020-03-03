45914676514b20a39dc47b51cd6544016f1d6b0f
Bill <207559339@qq.com>
2020-03-03 13:38:19 +0800

    game(without winning)

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,22 +3,38 @@ import React from "react"
 const style = {
     height: 30,
     width: 30,
-    fontSize: 10,
+    fontSize: 20,
     verticalAlign: "top"
 }
 
 class Box extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: ""
+        }
+    }
+
     setId = (num) => {
         let arr = [0, 0]
-        if(num>=15){
-            arr = [Math.floor(num/15), num%15]
-        }
-        return arr[0] + "-" + arr[1]
+        arr = [Math.floor(num/15), num%15]
+        return arr
     }
 
     render () { 
         return (
-            <button id={this.setId(this.props.id)} style={style}></button>
+            <button style={style} onClick={() => {
+                let locationArray = this.setId(this.props.id)
+                // console.log(locationArray)
+                if(this.props.game[locationArray[0]][locationArray[1]] === ""){
+                    this.setState({value: this.props.move})
+                    this.props.changeCashe(this.setId(this.props.id), this.props.move)
+                    this.props.changeSymbol()
+                    this.props.resetNum()
+                    console.log(this.props.game)
+                }
+            }}>{this.state.value}</button>
         )
     }
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -15,11 +15,38 @@ for (let ii = 0; ii < 225; ii++){
 let num = -1
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            nextMove: "X",
+            squareTaken: [["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]]
+        }
+    }
+
+    changeSymbol = () => {
+        if (this.state.nextMove === "X"){
+            this.setState({nextMove: "O"})
+        } else {
+            this.setState({nextMove: "X"})
+        }
+    }
+
+    changeCashe = (arr, symbol) => {
+        let squareTaken1 = this.state.squareTaken
+        squareTaken1[arr[0]][arr[1]] = symbol
+        this.setState({squareTaken: squareTaken1})
+    }
+
     createBox = () => {
         num += 1
-        return <Box id={num}></Box>
+        return <Box id={num} resetNum = {this.resetNum} changeCashe={this.changeCashe} move={this.state.nextMove} game={this.state.squareTaken} changeSymbol={this.changeSymbol}></Box>
     }    
-    
+
+    resetNum = () => {
+        num = -1
+    }
+
     createBoxes = () => {
         return arr1.map(this.createBox)
     }
