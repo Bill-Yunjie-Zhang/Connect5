a21c1ce193446acc3cdf008d1df81369bf575081
Bill <207559339@qq.com>
2020-03-03 15:59:58 +0800

    Updates

---
+++ precache-manifest.f510155832e660add1fc8dd288ff63c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f6cc6a2f3a9cf02e7cd7475040c832",
+    "url": "/Connect5/index.html"
+  },
+  {
+    "revision": "8cfabfa6791c97d9ccfc",
+    "url": "/Connect5/static/js/2.246729e5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Connect5/static/js/2.246729e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a018c39fd61732125f",
+    "url": "/Connect5/static/js/main.b58fbd76.chunk.js"
+  },
+  {
+    "revision": "149d32e59c64d436cfe9",
+    "url": "/Connect5/static/js/runtime-main.58c3740d.js"
+  }
+]);
